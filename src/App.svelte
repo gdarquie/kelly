5fa235e6c0c9cdb31bdf56e633dc36376239148f
gdarquie <gaetan.darquie@gmail.com>
2020-08-06 13:03:58 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Login from './components/Login.svelte';
+</script>
+
+<main>
+	<Login/>
+</main>
+
+<style>
+</style>
---
+++ src/components/Login.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Button, Checkbox } from 'svelte-mui';
+    let checked = true;
+</script>
+
+<div>
+    This is a login page
+    <input type="text"/>
+    <input type="password"/>
+    <button>Submit</button>
+</div>
+
+<Checkbox bind:checked>Checkbox</Checkbox>
+<Button
+outlined
+shaped
+color="Red"
+on:click={() => { checked = !checked }}
+>
+Inverse
+</Button>
